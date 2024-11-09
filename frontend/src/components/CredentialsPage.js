2398d8e2308e64a64a30484be7b3a6aa5472260c
Neerajgowda2611 <neerajgowda2611@gmail.com>
2024-11-09 19:31:45 +0530

    changed for response

--- frontend/src/components/CredentialsPage.js
+++ frontend/src/components/CredentialsPage.js
@@ -1,38 +1,54 @@
 // src/components/CredentialsPage.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'; // Import useParams hook
-import '/home/silent/Desktop/services/frontend/src/App.css'; // Import shared CSS for styling
+import '/home/silent/Desktop/services/frontend/src/App.css'; 
+import axios from "axios";// Import shared CSS for styling
 
 const CredentialsPage = () => {
   const { service } = useParams(); // Get the service name from the URL
+  const [user, setUser ] = useState([]);
+
+
+  const fetchUserDetails = async() => {
+    try {
+      const res = await axios.get("https://authtest.cialabs.org/api/get-user?userId=7b159c0d-a8c2-4ed1-a04e-68eb145ea938")
+      setUser(res?.data?.data )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserDetails()
+  }, [])
 
   return (
     <div className="credentials-page">
       <h2>{service} Credentials</h2>
       <div className="credentials-table">
         <div className="credentials-row">
-          <span className="credentials-label">User ID:</span>
-          <span className="credentials-value">qwertyuisdfghjkdfghrtyujhkj</span>
+          <span className="credentials-label">user</span>
+          <span className="credentials-value">{user?.id}</span>
         </div>
         <div className="credentials-row">
           <span className="credentials-label">Email:</span>
-          <span className="credentials-value">exampleexample@example.com</span>
+          <span className="credentials-value">{user?.email}</span>
         </div>
         <div className="credentials-row">
           <span className="credentials-label">Name:</span>
-          <span className="credentials-value">John Doe</span>
+          <span className="credentials-value">{user?.name}</span>
         </div>
         <div className="credentials-row">
           <span className="credentials-label">Created Time:</span>
-          <span className="credentials-value">2024-11-08 12:34:56</span>
+          <span className="credentials-value">{user?.createdTime}</span>
         </div>
         <div className="credentials-row">
           <span className="credentials-label">Access Key:</span>
-          <span className="credentials-value">-</span>
+          <span className="credentials-value">{user?.accessKey}</span>
         </div>
         <div className="credentials-row">
           <span className="credentials-label">Access Secret:</span>
-          <span className="credentials-value">-</span>
+          <span className="credentials-value">{user?.accessSecret}</span>
         </div>
       </div>
       <button className="generate-btn">Generate Key and Secret</button>
