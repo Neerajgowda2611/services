7f41de75a2ddcd79416578139014bb20d9df7151
Neerajgowda2611 <neerajgowda2611@gmail.com>
2024-11-11 20:02:06 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,3 @@
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import WelcomePage from './components/WelcomePage';
-// import ServicesPage from './components/ServicesPage';
-// import AboutPage from './components/AboutPage';
-// import CredentialsPage from './components/CredentialsPage';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<WelcomePage />} />
-//         <Route path="/services" element={<ServicesPage />} />
-//         <Route path="/about/:service" element={<AboutPage />} />
-//         <Route path="/credentials/:service" element={<CredentialsPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
@@ -34,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/service" element={<ServicesPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/credentials" element={<CredentialsPage />} />
       </Routes>
